fdcc116d6663dda8cd2111aa5c83dc261af29ad4
Ritesh Jena <ritesh@Riteshs-MacBook-Pro.local>
2019-01-06 18:36:41 +0530

    upload base files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import style from './style.scss'
+
+const root = document.createElement("div");
+root.innerHTML = `<p>Hello Learning Enthusiast.</p>`;
+document.body.appendChild(root);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules : [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      hash: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+}
